e306da741f8f1c29754a26d3779c630be5861202
cduggan <cduggan@reapit.com>
2025-03-06 14:17:12 +0000

    feat: add fluent style test assertions for exceptions

--- src/Reapit.Platform.Testing/Fluent/AssertionExtensions.cs
+++ src/Reapit.Platform.Testing/Fluent/AssertionExtensions.cs
@@ -1,10 +1,23 @@
-﻿using Reapit.Platform.Testing.Fluent.Assertions.Primitives;
+﻿using Reapit.Platform.Testing.Fluent.Assertions.Delegates;
+using Reapit.Platform.Testing.Fluent.Assertions.Primitives;
 
 namespace Reapit.Platform.Testing.Fluent;
 
 /// <summary>Extension methods for the creation of assertions objects.</summary>
 public static class AssertionExtensions
 {
+    #region Delegate Assertions
+
+    /// <summary>Create the assertions object for a <see cref="Action"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static ActionAssertions Must(this Action subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a <see cref="Func{T}"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static FuncAssertions<T> Must<T>(this Func<T> subject) => new(subject);
+    
+    #endregion
+    
     #region Numeric Assertions
     
     /// <summary>Create the assertions object for a <see cref="byte"/> object.</summary>
@@ -55,9 +68,49 @@ public static class AssertionExtensions
     
     #region Nullable Numeric Assertions
     
+    /// <summary>Create the assertions object for a nullable <see cref="byte"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<byte> Must(this byte? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="sbyte"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<sbyte> Must(this sbyte? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="short"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<short> Must(this short? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="ushort"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<ushort> Must(this ushort? subject) => new(subject);
+    
     /// <summary>Create the assertions object for a nullable <see cref="int"/> object.</summary>
     /// <param name="subject">The subject of the assertions.</param>
     public static NullableNumericAssertions<int> Must(this int? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="uint"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<uint> Must(this uint? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="long"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<long> Must(this long? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="ulong"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<ulong> Must(this ulong? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="float"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<float> Must(this float? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="double"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<double> Must(this double? subject) => new(subject);
+    
+    /// <summary>Create the assertions object for a nullable <see cref="decimal"/> object.</summary>
+    /// <param name="subject">The subject of the assertions.</param>
+    public static NullableNumericAssertions<decimal> Must(this decimal? subject) => new(subject);
     
     #endregion'
 }
